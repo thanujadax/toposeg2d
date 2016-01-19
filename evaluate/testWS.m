3b96561fc56df9eb9d5f0294e262ebdfd607a199
Thanuja Ambegoda <thanujadax@gmail.com>
2016-01-19 19:36:15 +0100

    added script to generate ws neuron segments for comparison

---
+++ evaluate/testWS.m
@@ -0,0 +1,19 @@
+% read probability map
+inputFileName = '/home/thanuja/projects/data/toyData/set8/membranes/08_schmidhuber_membrane.tiff';
+inputImage = readTiffStackToArray(inputFileName);
+figure;imagesc(inputImage)
+% ws
+ws_input = watershed(inputImage);
+figure;imagesc(ws_input);
+% gaussian filter
+sigma = 0.5;
+maskSize = 5;
+smoothProbMap = gaussianFilter(inputImage,sigma,maskSize);
+figure;imagesc(smoothProbMap)
+% ws
+ws_smooth = watershed(smoothProbMap);
+ws_smooth_r = assignRandomIndicesToWatershedTransform(ws_smooth);
+ws_rgb = indexImg2rgbImg(ws_smooth_r);
+figure;imagesc(ws_rgb);
+
+imwrite(ws_rgb,'watershedSegments.png','png');

d3de1bd9f020ab755139cac43d0e99d406a22f7e
Thanuja Ambegoda <thanujadax@gmail.com>
2016-07-11 19:16:13 +0200

    slices obtained

--- 3Dreconstruction/main_3Dreconstruction.m
+++ 3Dreconstruction/main_3Dreconstruction.m
@@ -8,14 +8,25 @@ outputDir = '/home/thanuja/projects/RESULTS/3Dreconstructions/20160711';
 %% Params
 
 %%
-
+inputFileList = dir(fullfile(inputDir,'*.tif'));
+numFiles = length(inputfileList);
+slices = struct([]);
+% 'slices' is a structure array with the following fields
+%       slices(i).sectionID
+%       slices(i).sliceID
+%       slices(i).pixelInds
+for i=1:numFiles
 % get slices from input 2D segments
-imageFileName = '/home/thanuja/projects/data/toyData/set8.2/groundtruth/00.tif';
-sectionID = '00';
+    imageFileName = fullfile(inputDir,inputFileList(i).name);
+    sectionID = sprintf('%03d',i);
+    slicesNext = getSlicesFromSection(imageFileName,sectionID);
+    % returns a structure array containing slices for the given section
+    slices = [slices, slicesNext];
+end
 
-c_s_slices = getSlicesFromSection(imageFileName,sectionID);
-% returns a structure array containing slices for the given section
+% define variables
 
+%% ILP
 % ilpObjective =  get3DILPobjective();
 % [constraintsA, constraintsB, constraintsSense] = get3DILPConstraints();
 % solutionVector = solve3DILPGurobi(ilpObjective,constraintsA,constraintsB,...

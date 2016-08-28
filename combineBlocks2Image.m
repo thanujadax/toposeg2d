37dbc9b403302d7f3b45745c460a5e0a72329131
Thanuja Ambegoda <thanuja@ini.ch>
2016-08-28 22:57:07 +0200

    combining chunks ctd ...

--- combineBlocks2Image.m
+++ combineBlocks2Image.m
@@ -1,18 +1,23 @@
-function imCombined = combineBlocks2Image(blockDir,outputPathPNG,blockDirName,...
+function canvas = combineBlocks2Image(blockDir,outputPathPNG,blockDirName,...
     rawImageIDstr,rOverlap,cOverlap,sizeR,sizeC,blockFileType)
 
-imCombined = zeros(sizeR,sizeC);
+canvas = zeros(sizeR,sizeC);
 
 % read all the images in the blockDir
 blockFilesDirList = dir(fullfile(blockDir,blockFileType));
 
+% replace the pixel value of the canvas with max(currentVal,blockVal)
 
 for i=1:numel(blockFilesDirList)    
     % make black and white images (foreground background)
     % stitch them together using min values at borders
     % assign neuron IDs (unique colors)
     im_i = imread(fullfile(blockDir,blockFilesDirList(i).name));
-    im_i = im2bw(im_i, 0);
+    im_i = im2bw(im_i, 0); % any value above zero is replaced by 1
+    % get the corner coordinates of the block wrt canvas
+    [rStart,rStop,cStart,cStop] = getBlockPositionForCanvas();
+    % max fill
+    canvas = maxFillCanvas(canvas,im_i,rStart,rStop,cStart,cStop);
     
 end
 

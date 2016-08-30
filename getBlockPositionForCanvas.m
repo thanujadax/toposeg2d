2d7a0a9c2146533cd51bc4d11bd995646cdfdb52
Thanuja Ambegoda <thanujadax@gmail.com>
2016-08-30 11:25:47 +0200

    chunking ctd ..

--- getBlockPositionForCanvas.m
+++ getBlockPositionForCanvas.m
@@ -1,8 +1,17 @@
-function [rStart,rStop,cStart,cStop] = getBlockPositionForCanvas(blockFileName)
+function [rStart,rStop,cStart,cStop] = getBlockPositionForCanvas...
+    (blockFileName,blockSizeR,blockSizeC,sizeR,sizeC)
 
 blockName = strtok(blockFileName,'.');
-cc = strsplit(blockName,'c')
+cc = strsplit(blockName,'c');
 blockColID = cc{2};
 dd = strsplit(cc{1},'r');
 blockRowID = dd{2};
+
+rStart = (blockRowID-1)*blockSizeR + 1;
+rStop = rStart + blockSizeR - 1;
+rStop = min(rStop,sizeR);
+
+cStart = (blockColID-1)*blockSizeC + 1;
+cStop = cStart + blockSizeC -1;
+cStop = min(cStop,sizeC);
 

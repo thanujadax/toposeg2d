997cdc9cf26500731e8a980ceed21302811920b2
Thanuja Ambegoda <thanujadax@gmail.com>
2016-08-31 19:01:18 +0200

    cluster script

---
+++ cremi_jobs.sh
@@ -0,0 +1,3 @@
+bsub matlab -nodisplay -nojvm -singleCompThread -r batchRun_cremi_A.m
+bsub matlab -nodisplay -nojvm -singleCompThread -r batchRun_cremi_B.m
+bsub matlab -nodisplay -nojvm -singleCompThread -r batchRun_cremi_C.m

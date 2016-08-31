2262d0023373d78009dc6d4eba8456b5bbfc65ac
Thanuja Ambegoda <thanujadax@gmail.com>
2016-08-31 18:54:57 +0200

    path updater

---
+++ updatePathCremi.m
@@ -0,0 +1,5 @@
+function updatePathCremi()
+addpath('/cluster/home/thanujaa/projects/contourdetection', 'subdirectories');
+addpath('/cluster/home/thanujaa/projects/common', 'subdirectories');
+addpath('/cluster/home/thanujaa/projects/matlabprojects', 'subdirectories');
+

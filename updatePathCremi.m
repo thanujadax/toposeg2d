1eec8f83f0ad843f63b2f7e6b3610a680f8bca17
Thanuja Ambegoda <thanujadax@gmail.com>
2016-10-05 10:19:11 +0200

    arton gurobi path added

--- updatePathCremi.m
+++ updatePathCremi.m
@@ -2,4 +2,5 @@ function updatePathCremi()
 addpath(genpath('/home/thanujaa/projects/contourdetection'));
 addpath(genpath('/home/thanujaa/projects/common'));
 addpath(genpath('/home/thanujaa/projects/matlabprojects'));
+addpath(genpath('/usr/pack/gurobi-6.5.1-sa/gurobi651/linux64/matlab'));
 

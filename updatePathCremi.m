72a4db0c14280c0d670c72ed82f3efb6cceee54d
Thanuja Ambegoda <thanujadax@gmail.com>
2016-10-20 17:10:10 +0200

    removed matlabprojects/RF from search path

--- updatePathCremi.m
+++ updatePathCremi.m
@@ -3,4 +3,5 @@ addpath(genpath('/home/thanujaa/projects/contourdetection'));
 addpath(genpath('/home/thanujaa/projects/common'));
 addpath(genpath('/home/thanujaa/projects/matlabprojects'));
 addpath(genpath('/usr/pack/gurobi-6.5.1-sa/gurobi651/linux64/matlab'));
+rmpath('/home/thanujaa/projects/matlabprojects/RF')
 
